6eb175b1a98464cfd6873d0f1bdbe2911cb49e34
Johan Janse van Rensburg <johanjvrens@gmail.com>
2015-07-18 19:41:41 +0200

    Refactored Shell Script to include option for running without the DocCheck. Restructured menu order. General cleanup of code.

--- sunore.sh
+++ sunore.sh
@@ -19,25 +19,29 @@ EOF
   echo "============================================================"
   echo "Select from the following functions.              q to EXIT."
   echo "============================================================"
-  echo "1 - Clean Workspace                                         "
-  echo "2 - Run DocCheck                                            "
-  echo "3 - Compile R Code                                          "
-  echo "4 - Compile PDF                                             "
-  echo "5 - Show PDF                                                "
-  echo "6 - All of the above                                        "
+  echo "1 - Build everything [3-7]                                  "
+  echo "2 - Build (without DocCheck) [3,5,6,7]                      "
+  echo "3 - Clean workspace                                         "
+  echo "4 - Run DocCheck                                            "
+  echo "5 - Compile R Code                                          "
+  echo "6 - Compile PDF                                             "
+  echo "7 - Show PDF                                                "
   echo "q - EXIT                                                    "
   read -r -p "Enter your choice [1-6] : " answer
+  echo "============================================================"
   case "$answer" in
-    1)  make clean ;;
-    2)  make check ;;
-    3)  make code  ;;
-    4)  make pdf   ;;
-    5)  make show  ;;
-    6)  make ;;
+    1)  make ;;
+    2)  make build ;;
+    3)  make clean ;;
+    4)  make check ;;
+    5)  make code  ;;
+    6)  make pdf   ;;
+    7)  make show  ;;
     "Q")  exit                      ;;
     "q")  exit                      ;;
     * )   echo "invalid option"     ;;
   esac
+  echo "============================================================"
   echo "Enter return to continue \c"
   read input
   done
